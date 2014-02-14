f1280fffe3ccfd380b8ddd19a637ffcfd35afd3e
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-02-14 12:25:30 +0000

    Added forever to restart on save

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   exphbs  = require('express3-handlebars'),
   app = express(),
-  routes = require('./routes');
+  routes = require('./routes'),
+  path = require('path');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -9,6 +10,7 @@ app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
 
---
+++ dev-app.js
@@ -0,0 +1,15 @@
+var forever = require("forever-monitor");
+
+var child = new (forever.Monitor)("app.js", {
+  max: -1,
+  silent: false,
+  options: [],	
+  watch: true,
+  watchDirectory: "./"
+});
+
+child.on("exit", function () {
+  console.log("app.js has exited");
+});
+
+child.start();
