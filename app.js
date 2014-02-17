33b36cb99eee50cef09a82aaba0172adcdff60b5
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-02-17 22:54:38 +0000

    Added ldap authentication and hooked up auth service to login form

--- app.js
+++ app.js
@@ -5,16 +5,30 @@ var express = require('express'),
   authenticateroutes = require('./routes/authenticate'),
   path = require('path');
 
+var authenticationChecker = function(req, res, next) { 
+  if (req.session.user && req.session.user.isAuthenticated) {
+    next();   
+  } else {
+    res.redirect('/');   
+  }
+};
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+app.use(express.bodyParser());
+app.use(express.cookieParser('featuretoggle'));
+app.use(express.session());
 
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/dashboard', dashboardroutes.index);
+app.get('/dashboard', authenticationChecker, dashboardroutes.index);
 app.get('/', authenticateroutes.authenticate);
+
+app.post('/', authenticateroutes.login);
 
 console.log('Starting up feature toggle dashboard');
 
--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,3 +1,22 @@
-exports.authenticate = function(req, res){
+var authenticationService = require('./../src/services/authentication_service.js');
+
+exports.authenticate = function(req, res) {
   res.render('authenticate', { layout: 'authenticate', title: 'Feature toggles: Authenticate', pageHeader: 'Authenticate' });
+};
+
+exports.login = function(req, res) {
+    console.log('logging in...');
+    console.log('username is: ' + req.body.username);
+    authenticationService.login(req.body.username, req.body.password, function(result) {
+        console.log('Authenticated: ' + result);
+        if (result) {
+            req.session.user = { isAuthenticated: true, username: req.body.username };
+            res.redirect('/dashboard');
+        }
+        else{
+            res.redirect('/');
+        }
+        
+    });
+    
 };
