e880627b6fb0379ce9d2a0d9a1b2e77134488161
Ryan Tomlinson <tomlinsonryan@gmail.com>
2014-08-21 19:23:09 +0100

    Added lbstatus route and file read

---
+++ server/routes/loadbalancerRoutes.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+exports.lbstatus = function(req, res) {
+  fs.readFile('/etc/lbstatus/hobknob', 'utf8', function(err, data) {
+    if (err) {
+      res.send(500, err);
+    } 
+    else {
+      res.send(data);
+    }
+  });
+};
