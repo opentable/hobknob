a2c1a32a7b7f60558b1ecf071732469b7b6817f1
Chris Ridde <criddle@opentable.com>
2014-11-03 13:19:39 +0000

    Updated test path

--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -1,4 +1,4 @@
 SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 
 
-node node_modules/protractor/bin/protractor client/tests/protractor.conf.js
+node node_modules/protractor/bin/protractor client/tests/e2e/protractor.conf.js
