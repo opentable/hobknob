056aecfd6e403b5b332a8918af4d6976eafad582
Chris Ridde <criddle@opentable.com>
2014-11-03 17:20:54 +0000

    Removed crazy reverse of access key - fuck knows why that was there

--- client/tests/e2e/protractor.conf.js
+++ client/tests/e2e/protractor.conf.js
@@ -18,10 +18,7 @@ if (process.env.TRAVIS_BUILD_NUMBER) {
 	config.sauceUser = process.env.SAUCE_USERNAME;
 	config.sauceKey = process.env.SAUCE_ACCESS_KEY;
     config.capabilities = {
-        'username': process.env.SAUCE_USERNAME,
-        'accessKey': process.env.SAUCE_ACCESS_KEY,
         'browserName': 'chrome',
-        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
         'build': process.env.TRAVIS_BUILD_NUMBER,
         'name': 'Hobknob Tests'
     };
--- scripts/testonsauce.sh
+++ scripts/testonsauce.sh
@@ -1,4 +1,1 @@
-SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
-
-
 node node_modules/protractor/bin/protractor client/tests/e2e/protractor.conf.js
