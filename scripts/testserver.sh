a57a0df2cb33fd70502b8d61c520cdd82d04e295
Chris Ridde <criddle@opentable.com>
2014-11-04 10:39:01 +0000

    Added bower install

--- scripts/testserver.sh
+++ scripts/testserver.sh
@@ -2,6 +2,8 @@
 
 # Start up the server in a way that won't block Travis.
 
+bower cache clean --config.interactive=false && bower install --config.interactive=false
+
 node server/app.js &
 sleep 1
 echo Hobknob started
