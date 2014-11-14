6ad7ccb86acbdfd8930a47d55efc649b8c8c26c8
christriddle <christriddle@gmail.com>
2014-11-14 15:47:12 +0000

    Delete feature tests now work

--- client/tests/e2e/protractor.conf.js
+++ client/tests/e2e/protractor.conf.js
@@ -5,7 +5,7 @@ var config = {
     },
     baseUrl: 'http://127.0.0.1:3006/',
     framework: 'jasmine',
-    specs: ['**/new*.spec.js'],
+    specs: ['**/del*.spec.js'],
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000,
