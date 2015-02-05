4a86a28564db4c171f7d91f707e676d44052ede5
Federico Maffei <federico.maffei@gmail.com>
2015-02-05 17:30:31 +0000

    added e2e test for description update, all tests passing.

---
+++ client/tests/e2e/update-description.spec.js
@@ -0,0 +1,47 @@
+var Etcd = require('node-etcd'),
+    etcd = new Etcd();
+
+describe('Update a feature description', function () {
+    var protractorInstance = protractor.getInstance();
+
+    var descCss = '.editable';
+    var editableInputCss = '.editable-input';
+
+    var removeAllFeatures = function(done){
+        etcd.rmdir('v1', { recursive: true }, function(){
+            done();
+        });
+    };
+
+    beforeEach(function(done) {
+        removeAllFeatures(function() {
+            etcd.set('v1/metadata/TestApp/githubRepoUrl', 'https://github.com/opentable/hobknob', function () {
+                etcd.set('v1/toggles/TestApp/TestFeature', false, function () {
+                    etcd.set('v1/metadata/TestApp/descriptions/TestFeature', 'text', function (){
+                        browser.get('/#!/applications/TestApp/TestFeature');
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    afterEach(function(done){
+        removeAllFeatures(done);
+    });
+
+    var getDesc = function(){
+        return element(by.css(descCss));
+    };
+
+    var expectDescToHaveText = function(state){
+        expect(getDesc().getText()).toMatch('edited text');
+    };
+
+    it('should be able to update a description to the expected value', function(){
+        getDesc().click();
+        element(by.css(editableInputCss)).sendKeys('edited text');
+        protractorInstance.actions().sendKeys(protractor.Key.ENTER).perform();
+        expectDescToHaveText(true);
+    });
+});
