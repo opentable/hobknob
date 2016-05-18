2fad8e0f8e5a5b50d51258f983aaf6235aa54bba
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2016-05-18 13:49:09 +0300

    Including windowsn auth support

--- server/auth.js
+++ server/auth.js
@@ -2,14 +2,16 @@
 
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var Office365Strategy = require('passport-azure-ad-oauth2').AzureAdOAuth2Strategy;
 
 module.exports.init = function (config) {
     if (config.RequiresAuth) {
+        var protocolSection = (config.hobknobProtocol) ? config.hobknobProtocol : 'http';
+        var portSection = (config.hobknobPort) ? ':' + config.hobknobPort : '';
+
         if (config.AuthProviders.GoogleAuth) {
             var GOOGLE_CLIENT_ID = config.AuthProviders.GoogleAuth.GoogleClientId;
             var GOOGLE_CLIENT_SECRET = config.AuthProviders.GoogleAuth.GoogleClientSecret;
-            var protocolSection = (config.hobknobProtocol) ? config.hobknobProtocol : 'http';
-            var portSection = (config.hobknobPort) ? ':' + config.hobknobPort : '';
             var googleCallbackURL = protocolSection + '://' + config.hobknobHost + portSection + '/auth/google/callback';
 
             passport.use(new GoogleStrategy({
@@ -23,6 +25,25 @@ module.exports.init = function (config) {
                         return done(null, profile);
                     });
                 }));
+        } else if (config.AuthProviders.AzureAuth)
+        {
+          var AZURE_CLIENT_ID = config.AuthProvider.AzureAuth.AzureClientId;
+          var AZURE_CLIENT_SECRET = config.AuthProviders.AzureAuth.AzureClientSecret;
+          var AZURE_TENANT_ID = config.AuthProvider.AzureAuth.AzureTenantId;
+          var azureCallbackURL = protocolSection + '://' + config.hobknobHost + portSection + '/auth/azure/callback';
+
+          passport.use(new Office365Strategy({
+                    clientID: AZURE_CLIENT_ID,
+                    clientSecret: AZURE_CLIENT_SECRET,
+                    callbackURL: azureCallbackURL,
+                    tenant: AZURE_TENANT_ID
+                  },
+                  function (accessToken, refreshToken, params, profile, done) {
+                      profile.accessToken = accessToken;
+                      process.nextTick(function() {
+                            return done(null, profile);
+                      });
+                  }));
         }
     }
 
