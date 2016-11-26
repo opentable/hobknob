12bfa1a6115bd04d5f90dd9cb7fbc5dd89cd9d07
tombuildsstuff <git@ibuildstuff.co.uk>
2016-11-26 15:33:45 +0000

    Moving the etcdD data source settings in the config

--- server/domain/etcd/etcd.js
+++ server/domain/etcd/etcd.js
@@ -3,4 +3,4 @@
 var config = require(__base + '/../config/server.json');
 var Etcd = require('node-etcd');
 
-module.exports.client = new Etcd(config.etcdHost, config.etcdPort);
+module.exports.client = new Etcd(config.dataSources.etcd.host, config.dataSources.etcd.port);
