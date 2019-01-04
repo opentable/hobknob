bbf183d20014a752dfb91d4ffd430df402e13eab
Fernando de Morais <mnando@gmail.com>
2019-01-04 13:12:56 -0200

    Using npm and grunt to run ci commands

--- scripts/testserver.sh
+++ scripts/testserver.sh
@@ -2,8 +2,6 @@
 
 # Start up the server in a way that won't block Travis.
 
-bower cache clean --config.interactive=false && bower install --config.interactive=false
-
-node server/app.js &
+npm start &
 sleep 1
 echo Hobknob started
