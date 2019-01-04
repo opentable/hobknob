ffb12894b9c2fadf760b9835aad01c086d1ae98e
Fernando de Morais <mnando@gmail.com>
2019-01-04 13:12:56 -0200

    Using npm and grunt to run ci commands

--- scripts/rune2etest.sh
+++ scripts/rune2etest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -s http://localhost:3006/ > /dev/null # poking the server to wake it up before starting tests.
-node node_modules/protractor/bin/protractor client/tests/e2e/protractor.conf.js
+grunt protractor
