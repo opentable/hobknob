2dac9dad297c1358aa832f100b03f18eaa1c1f67
Fernando de Morais <mnando@gmail.com>
2019-01-04 12:29:16 -0200

    Updating Travis configurarion.

--- client/tests/e2e/protractor.conf.js
+++ client/tests/e2e/protractor.conf.js
@@ -1,28 +1,20 @@
-var config = {
-    capabilities: {
-        'browserName': 'chrome',
-        'version': '',
-        'platform': 'ANY'
-    },
-    baseUrl: 'http://127.0.0.1:3006/',
-    framework: 'jasmine2',
-    specs: ['**/*.spec.js'],
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000,
-        isVerbose: true,
-        includeStackTrace: true
-    }
-};
+'use strict';
 
-if (process.env.TRAVIS) {
-    config.capabilities = {
-        'browserName': 'firefox',
-        'version': '',
-        'platform': 'ANY',
-        'build': process.env.TRAVIS_BUILD_NUMBER,
-        'name': 'Hobknob Tests'
-    };
-}
+var config = {
+  capabilities: {
+    browserName: 'chrome',
+    version: '',
+    platform: 'ANY'
+  },
+  baseUrl: 'http://127.0.0.1:3006/',
+  framework: 'jasmine2',
+  specs: ['**/*.spec.js'],
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    isVerbose: true,
+    includeStackTrace: true
+  }
+};
 
 exports.config = config;
